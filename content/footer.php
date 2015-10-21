f3d4ed2b8d0530bb804f79b7080aaf7e532f7882
Luis Manuel Deras <lmrderas@gmail.com>
2015-10-20 22:07:56 -0600

    Definición HTML, Incluyendo librería Font Awesome

---
+++ content/footer.php
@@ -0,0 +1,15 @@
+<footer>
+
+<div class="row">
+  <div class="col-lg-4">
+    <i class="fa fa-twitter fa-4x"></i>
+  </div>
+  <div class="col-lg-4">
+
+  </div>
+  <div class="col-lg-4">
+
+  </div>
+</div>
+
+</footer>
---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="Turismo en Honduras">
+		<meta name="keywords" content="turismo, Honduras, hoteles, vacaciones, paisajes, Roatan, Utila, Guanaja, Islas de la Bahia, ruinas de Copan, Copan, Omoa, castillo San Fernando de Omoa, Tela, Ceiba, Tegucigalpa, San Pedro Sula, canopy, Lempira, playas, ruinas, cayos cochinos, centroamerica, america latina, latinoamerica">
+		<title>Turismo en Honduras</title>
+
+		<link rel="icon" href="hn-icon.png" type="image">
+
+		<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css" media="screen" title="Bootstrap CSS" charset="utf-8">
+		<link rel="stylesheet" href="css/estilo.css" media="screen" title="Bootstrap CSS" charset="utf-8">
+		<link rel="stylesheet" href="lib/fontAwesome/css/font-awesome.min.css" media="screen" title="Font Awesome Icons" charset="utf-8">
+
+	</head>
+
+	<body>
+
+		<div class="container">
+
+			<ul class="nav nav-tabs">
+			  <li class="nav-item">
+			    <a href="index.html" class="nav-link active">Active</a>
+			  </li>
+			  <li class="nav-item">
+			    <a href="#" class="nav-link">Link</a>
+			  </li>
+			  <li class="nav-item">
+			    <a href="#" class="nav-link">Another link</a>
+			  </li>
+			  <li class="nav-item disabled">
+			    <a href="#" class="nav-link">Disabled</a>
+			  </li>
+			</ul>
+
+			<div class="row">
+				<div class="col-lg-12">
+
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-3">
+					<div class="list-group">
+						<div class="list-group-item list-group-item-success puntero-manito">
+							<h3 class="list-group-item-heading"></h3>
+							<p class="text-center">
+								<figure>
+									<img class="img-circle  img-media" src="image.jpg">
+								</figure>
+							</p>
+							<p class="text-justify">
+								texto texto texto texto texto texto texto texto texto texto texto texto texto
+								texto texto texto texto texto texto texto texto texto texto texto texto texto
+								texto texto texto texto texto texto texto texto texto texto texto texto
+							</p>
+						</div>
+
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="list-group-item">
+						<h3 class="list-group-item-heading"></h3>
+						<p class="text-center">
+							<figure>
+								<img class="media-object img-circle  img-media" src="image.jpg">
+
+							</figure>
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="list-group-item">
+						<h3 class="list-group-item-heading"></h3>
+						<p class="text-center">
+							<figure>
+								<img class="media-object img-circle  img-media" src="image.jpg">
+
+							</figure>
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="list-group-item">
+						<h3 class="list-group-item-heading"></h3>
+						<p class="text-center">
+							<figure>
+								<img class="media-object img-circle  img-media" src="image.jpg">
+
+							</figure>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<?php require_once("content/footer.php"); ?>
+
+	</body>
+</html>
