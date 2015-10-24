4263e41896b106952133be03589e9bdb7ff68f09
Luis Manuel Deras <lmrderas@gmail.com>
2015-10-23 22:49:31 -0600

    Modificaciones en pagina mision y vision.
    Las animaciones aun no funcionan y falta otro icono para la vision.

--- js/js_index.js
+++ js/js_index.js
@@ -7,5 +7,10 @@ function animacionTarjeta()
 {
   //Obtener un numero random en base a las tarjetas
   var randomNumber = Math.floor(Math.random() *  $(".tarjeta").length);
+
+  /*
+  * //Generar animacion de la tarjeta con numero randomNumber
+  * y asegurarse que los hermanos no tengan la animacion
+  */
   $(".tarjeta").eq(randomNumber).addClass('tada').siblings().removeClass('tada');
 }
---
+++ js/js_misionVision.js
@@ -0,0 +1,25 @@
+// $(document()
+// {
+//
+// });
+
+function init()
+{
+  // $("#rowMision").hide();
+  // $("#rowVision").hide();
+  //
+  //
+  // $("#rowMision").fadeIn();
+  //
+  // setTimeout(function(){}, 10000000);
+  //
+  //
+  // $("#rowVision").fadeIn();
+
+  $("#rowVision").hide();
+  $("#rowVision").removeClass('hidden',
+  function()
+  {
+      $("#rowVision").fadeIn("slow");
+  });
+}
